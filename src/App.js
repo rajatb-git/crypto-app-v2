c0ee773c91fe04fbce0f0e7b7180b083f14a2963
Rajat Bansal <rb9322@gmail.com>
2020-01-19 15:21:41 -0600

    init

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Header from './components/Header/Header.jsx';
+
+function App() {
+  return (
+    <div id="root" className="App">
+        <Header />
+    </div>
+  );
+}
+
+export default App;
