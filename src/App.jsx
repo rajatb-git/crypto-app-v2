96ff33f5367884f172e7f6a54e97b6b1b0f15a30
Rajat Bansal <ramborajat@users.noreply.github.com>
2020-01-21 23:01:26 -0600

    Basic auth and routing setup (#2)
    * basic auth and routing completed
    
    * build fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 import Routing from './Routing/Router';
 import Header from './components/Header/Header';
+import { AuthContext } from './context/Auth';
 
 function App() {
+  const [authTokens, setAuthTokens] = useState(localStorage.getItem('tokens'));
+
+  const setTokens = (data) => {
+    localStorage.setItem('tokens', data);
+    setAuthTokens(data);
+  };
+
   return (
-    <div id="root" className="App">
-      <Header />
-      <Routing />
-    </div>
+    <AuthContext.Provider value={{ authTokens, setAuthTokens: setTokens }}>
+      <Router>
+        <Header />
+        <Routing />
+      </Router>
+    </AuthContext.Provider>
   );
 }
 
